a478be8cd9a28e4846327d30ed8170c662dd88cd
Torianyk Alex <torianyk.alex@gmail.com>
2022-08-24 19:39:18 +0300

    add

---
+++ src/JS/02-video.js
@@ -0,0 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+const parsed = JSON.parse(localStorage.getItem(currentTime));
+
+function onPlay(data) {
+  console.log(data);
+  localStorage.setItem(currentTime, JSON.stringify(data));
+}
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function returnPlayback() {
+  if (parsed === null) {
+    return;
+  }
+  const paused = parsed['seconds'];
+
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+returnPlayback();
---
+++ src/JS/03-feedback.js
@@ -0,0 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const key = 'feedback-form-state';
+const feedBack = document.querySelector('.feedback-form');
+const mail = document.querySelector('input[name=email]');
+const message = document.querySelector('textarea[name=message]');
+const btn = document.querySelector('button[type=submit]');
+
+feedBack.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+  const form = new FormData(feedBack);
+  let newForm = {};
+  form.forEach((value, name) => (newForm[name] = value));
+  localStorage.setItem(key, JSON.stringify(newForm));
+}
+
+function saveInf() {
+  const newFormInf = JSON.parse(localStorage.getItem(key));
+  if (newFormInf) {
+    const inputDatas = Object.keys(newFormInf);
+    inputDatas.forEach(inputData => {
+      feedBack.elements[inputData].value = newFormInf[inputData];
+    });
+  }
+}
+saveInf();
+
+feedBack.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  localStorage.removeItem(key);
+  let infData = {};
+  const formData = new FormData(feedBack);
+  formData.forEach((value, name) => (infData[name] = value));
+  console.log(infData);
+  feedBack.reset();
+}
